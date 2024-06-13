fcc0070e937077430678e64a68e197c348756aa7
olesiaa22 <alesyashpotak13@gmail.com>
2024-06-13 11:02:09 +0300

    first commit

---
+++ WinFormsApp6/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WinFormsApp6
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonToggleMode;
+        private System.Windows.Forms.Button buttonToggleSplitRegions;
+        private System.Windows.Forms.Button buttonShowMemory;
+        private System.Windows.Forms.Button buttonShowElapsedTime;
+        private System.Windows.Forms.Button buttonShowCpuTime;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonToggleMode = new System.Windows.Forms.Button();
+            this.buttonToggleSplitRegions = new System.Windows.Forms.Button();
+            this.buttonShowMemory = new System.Windows.Forms.Button();
+            this.buttonShowElapsedTime = new System.Windows.Forms.Button();
+            this.buttonShowCpuTime = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 600);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(12, 606);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGenerate.TabIndex = 1;
+            this.buttonGenerate.Text = "Generate";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+
+            // 
+            // buttonToggleMode
+            // 
+            this.buttonToggleMode.Location = new System.Drawing.Point(93, 606);
+            this.buttonToggleMode.Name = "buttonToggleMode";
+            this.buttonToggleMode.Size = new System.Drawing.Size(75, 23);
+            this.buttonToggleMode.TabIndex = 2;
+            this.buttonToggleMode.Text = "Toggle Mode";
+            this.buttonToggleMode.UseVisualStyleBackColor = true;
+            this.buttonToggleMode.Click += new System.EventHandler(this.buttonToggleMode_Click);
+
+            // 
+            // buttonToggleSplitRegions
+            // 
+            this.buttonToggleSplitRegions.Location = new System.Drawing.Point(174, 606);
+            this.buttonToggleSplitRegions.Name = "buttonToggleSplitRegions";
+            this.buttonToggleSplitRegions.Size = new System.Drawing.Size(140, 23);
+            this.buttonToggleSplitRegions.TabIndex = 3;
+            this.buttonToggleSplitRegions.Text = "Toggle Split Regions";
+            this.buttonToggleSplitRegions.UseVisualStyleBackColor = true;
+            this.buttonToggleSplitRegions.Click += new System.EventHandler(this.buttonToggleSplitRegions_Click);
+
+            // 
+            // buttonShowMemory
+            // 
+            this.buttonShowMemory.Location = new System.Drawing.Point(320, 606);
+            this.buttonShowMemory.Name = "buttonShowMemory";
+            this.buttonShowMemory.Size = new System.Drawing.Size(100, 23);
+            this.buttonShowMemory.TabIndex = 4;
+            this.buttonShowMemory.Text = "Show Memory";
+            this.buttonShowMemory.UseVisualStyleBackColor = true;
+            this.buttonShowMemory.Click += new System.EventHandler(this.buttonShowMemory_Click);
+
+            // 
+            // buttonShowElapsedTime
+            // 
+            this.buttonShowElapsedTime.Location = new System.Drawing.Point(426, 606);
+            this.buttonShowElapsedTime.Name = "buttonShowElapsedTime";
+            this.buttonShowElapsedTime.Size = new System.Drawing.Size(100, 23);
+            this.buttonShowElapsedTime.TabIndex = 5;
+            this.buttonShowElapsedTime.Text = "Show Elapsed Time";
+            this.buttonShowElapsedTime.UseVisualStyleBackColor = true;
+            this.buttonShowElapsedTime.Click += new System.EventHandler(this.buttonShowElapsedTime_Click);
+
+            // 
+            // buttonShowCpuTime
+            // 
+            this.buttonShowCpuTime.Location = new System.Drawing.Point(532, 606);
+            this.buttonShowCpuTime.Name = "buttonShowCpuTime";
+            this.buttonShowCpuTime.Size = new System.Drawing.Size(100, 23);
+            this.buttonShowCpuTime.TabIndex = 6;
+            this.buttonShowCpuTime.Text = "Show CPU Time";
+            this.buttonShowCpuTime.UseVisualStyleBackColor = true;
+            this.buttonShowCpuTime.Click += new System.EventHandler(this.buttonShowCpuTime_Click);
+
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 641);
+            this.Controls.Add(this.buttonShowCpuTime);
+            this.Controls.Add(this.buttonShowElapsedTime);
+            this.Controls.Add(this.buttonShowMemory);
+            this.Controls.Add(this.buttonToggleSplitRegions);
+            this.Controls.Add(this.buttonToggleMode);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Voronoi Diagram";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ WinFormsApp6/Form1.cs
@@ -0,0 +1,193 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> points = new List<Point>();
+        private Bitmap bitmap;
+        private bool multiThreaded = false;
+        private bool splitRegions = false;
+        private Random random = new Random();
+
+        private long memoryUsed;
+        private TimeSpan elapsedTime;
+        private TimeSpan cpuTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.MouseClick += PictureBox1_MouseClick;
+            this.pictureBox1.Paint += PictureBox1_Paint;
+        }
+
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (bitmap != null)
+            {
+                e.Graphics.DrawImage(bitmap, 0, 0);
+            }
+        }
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(e.Location);
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                points.RemoveAll(p => Math.Abs(p.X - e.X) < 5 && Math.Abs(p.Y - e.Y) < 5);
+            }
+            DrawVoronoi();
+        }
+
+        private void DrawVoronoi()
+        {
+            if (points.Count == 0) return;
+
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            if (multiThreaded)
+            {
+                if (splitRegions)
+                {
+                    int regionSize = 100; // Розмір області, який використовується для розбиття
+                    int regionsX = (pictureBox1.Width + regionSize - 1) / regionSize;
+                    int regionsY = (pictureBox1.Height + regionSize - 1) / regionSize;
+
+                    Parallel.For(0, regionsX, rx =>
+                    {
+                        Parallel.For(0, regionsY, ry =>
+                        {
+                            int startX = rx * regionSize;
+                            int startY = ry * regionSize;
+                            int endX = Math.Min(startX + regionSize, pictureBox1.Width);
+                            int endY = Math.Min(startY + regionSize, pictureBox1.Height);
+
+                            for (int x = startX; x < endX; x++)
+                            {
+                                for (int y = startY; y < endY; y++)
+                                {
+                                    var closestPoint = GetClosestPoint(new Point(x, y));
+                                    lock (bitmap)
+                                    {
+                                        bitmap.SetPixel(x, y, ColorFromPoint(closestPoint));
+                                    }
+                                }
+                            }
+                        });
+                    });
+                }
+                else
+                {
+                    Parallel.For(0, pictureBox1.Width, x =>
+                    {
+                        for (int y = 0; y < pictureBox1.Height; y++)
+                        {
+                            var closestPoint = GetClosestPoint(new Point(x, y));
+                            lock (bitmap)
+                            {
+                                bitmap.SetPixel(x, y, ColorFromPoint(closestPoint));
+                            }
+                        }
+                    });
+                }
+            }
+            else
+            {
+                for (int x = 0; x < pictureBox1.Width; x++)
+                {
+                    for (int y = 0; y < pictureBox1.Height; y++)
+                    {
+                        var closestPoint = GetClosestPoint(new Point(x, y));
+                        bitmap.SetPixel(x, y, ColorFromPoint(closestPoint));
+                    }
+                }
+            }
+
+            stopwatch.Stop();
+
+            var process = Process.GetCurrentProcess();
+            memoryUsed = process.PrivateMemorySize64;
+            elapsedTime = stopwatch.Elapsed;
+            cpuTime = process.TotalProcessorTime;
+
+            pictureBox1.Invalidate();
+        }
+
+        private Point GetClosestPoint(Point p)
+        {
+            Point closestPoint = points[0];
+            double closestDistance = GetDistance(p, closestPoint);
+
+            foreach (var point in points)
+            {
+                double distance = GetDistance(p, point);
+                if (distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestPoint = point;
+                }
+            }
+
+            return closestPoint;
+        }
+
+        private double GetDistance(Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
+
+        private Color ColorFromPoint(Point point)
+        {
+            int index = points.IndexOf(point);
+            return Color.FromArgb(255, (index * 50) % 255, (index * 80) % 255, (index * 110) % 255);
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            points.Clear();
+            int numPoints = 100; // кількість точок для генерації
+            for (int i = 0; i < numPoints; i++)
+            {
+                points.Add(new Point(random.Next(pictureBox1.Width), random.Next(pictureBox1.Height)));
+            }
+            DrawVoronoi();
+        }
+
+        private void buttonToggleMode_Click(object sender, EventArgs e)
+        {
+            multiThreaded = !multiThreaded;
+        }
+
+        private void buttonToggleSplitRegions_Click(object sender, EventArgs e)
+        {
+            splitRegions = !splitRegions;
+        }
+
+        private void buttonShowMemory_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Memory Used: {memoryUsed / 1024 / 1024} MB");
+        }
+
+        private void buttonShowElapsedTime_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Elapsed Time: {elapsedTime.TotalMilliseconds} ms");
+        }
+
+        private void buttonShowCpuTime_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"CPU Time: {cpuTime.TotalMilliseconds} ms");
+        }
+    }
+}
